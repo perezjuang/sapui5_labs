e5aec0cb6bd1a484423e222464ea367f5924be4f
BEYONDTECH\jperez <jperez@beyondtechnologies.ca>
2022-07-14 09:35:44 -0400

    NewView2

---
+++ com.labs.myUI5App/uimodule/webapp/controller/newview2.controller.js
@@ -0,0 +1,5 @@
+sap.ui.define(["com/labs/myUI5App/controller/BaseController"], function (Controller) {
+    "use strict";
+
+    return Controller.extend("com.labs.myUI5App.controller.newview2", {});
+});
