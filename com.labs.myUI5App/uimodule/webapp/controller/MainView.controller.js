496045a821dc7106e206943f4e8f9c7be4641c6c
BEYONDTECH\jperez <jperez@beyondtechnologies.ca>
2022-08-20 19:56:39 -0400

    labs

--- com.labs.myUI5App/uimodule/webapp/controller/MainView.controller.js
+++ com.labs.myUI5App/uimodule/webapp/controller/MainView.controller.js
@@ -1,13 +1,52 @@
 sap.ui.define(
-    ["./BaseController"],
+    ["./BaseController", "sap/m/MessageToast", "sap/ui/model/json/JSONModel", "sap/ui/model/resource/ResourceModel"],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
+     * @param {typeof sap.m.MessageToast} MessageToast
+     * @param {typeof sap.ui.model.json.JSONModel} JSONModel
      */
-    function (Controller) {
+    function (Controller, MessageToast, JSONModel) {
         "use strict";
 
         return Controller.extend("com.labs.myUI5App.controller.MainView", {
-            onInit: function () {},
+            onPress: function (oEvent) {
+                alert("Hello Nex To Next Page");
+                var oRouter = this.getOwnerComponent().getRouter();
+                oRouter.navTo("newview2");
+            },
+            onShowHello: function () {
+                // show a native JavaScript alert
+               
+
+                // read msg from i18n model
+                var oBundle = this.getView().getModel("i18n").getResourceBundle();
+                var sRecipient = this.getView().getModel().getProperty("/recipient/name");
+                var sMsg = oBundle.getText("helloMsg", [sRecipient]);
+                // show message
+                MessageToast.show(sMsg);
+
+                var oRouter = this.getOwnerComponent().getRouter();
+                oRouter.navTo("newview2");
+
+            },
+            onInit: function () {
+                // set data model on view
+               /* var oData = {
+                    recipient: {
+                        name: "World",
+                    },
+                };
+                var oModel = new JSONModel(oData);
+                console.log("/oM_functionallocation");
+                console.log(oModel);
+                this.getView().setModel(oModel);*/
+
+                var oModel = new JSONModel("products.json");
+                this.getView().setModel(oModel);
+                console.log(oModel);
+
+                
+            },
         });
     }
 );
